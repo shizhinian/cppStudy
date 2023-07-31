bfe5d37e13ba0266c96a170d6fd89eca47eff51e
XuRui <378007071@qq.com>
2023-07-31 15:11:36 +0800

    学习标准库类型string

---
+++ 标准库类型string.cpp
@@ -0,0 +1,84 @@
+//#include<string>
+//#include<iostream>
+//using std::string;
+//using std::cin;
+//using std::cout;
+//using std::endl;
+//
+//
+//int main() {
+//	//定义和初始化string对象
+//	/*
+//	string s1;
+//	string s2 = s1;
+//	string s3 = "xxx";//拷贝初始化
+//	string s4(10, 'x');//直接初始化
+//	string s5("xxxx");//直接初始化
+//	*/
+//	/*
+//	string s1, s2;
+//	cin >> s1 >> s2;
+//	cout << s1 << s2 << endl;
+//	*/
+//
+//	/*string word;
+//	while (cin >> word) {
+//		cout << word << endl;
+//
+//	}
+//	return 0;*/
+//
+//
+//	//string line;
+//	//auto len = line.size();//len:0 unsigned__int64
+//
+//	/*string str("some string");
+//	for (auto c : str)
+//		cout << c << endl;*/
+//
+//		/*string s("Hello World!!!");
+//		decltype(s.size())punct_cnt = 0;
+//
+//		for (auto c : s)
+//		{
+//			if (ispunct(c))
+//			{
+//				punct_cnt++;
+//			}
+//		}
+//		cout << punct_cnt << " punctuation characters in “" << s << "”"<<endl;*/
+//
+//		/*string s("Hello World!!!");
+//		for (auto &c:s)
+//		{
+//			c = toupper(c);
+//		}
+//		cout << s << endl;*/
+//
+//		/*string s("some string");
+//		if (!s.empty())
+//		{
+//			s[0] = toupper(s[0]);
+//		}
+//		cout << s << endl;*/
+//
+//		/*string s("some string");
+//		for (decltype(s.size()) index = 0; index!=s.size()&&!isspace(s[index]); ++index)
+//		{
+//			s[index] = toupper(s[index]);
+//		}
+//		cout << s << endl;*/
+//
+//	/*const string hexdigits = "0123456789ABCDEF";
+//	string result;
+//	string::size_type n;
+//	while (cin >> n)
+//	{
+//		if (n < hexdigits.size())
+//		{
+//			result += hexdigits[n];
+//		}
+//	}
+//	cout << result << '\n' << endl;
+//	return 0;*/
+//}
--- 自定义数据结构.cpp
+++ 自定义数据结构.cpp
@@ -1,53 +1,53 @@
-#include<iostream>
-//#include<string>
-#include"Sales_data.h"
-using namespace std;
-
-
-
-//Sales_data accum, trans, * salesptr;
-
-//练习2.39
-struct Foo {};
-//当没有写分号时，编译器报了以下错误：
-//错误	C2628	“Foo”后面接“int”是非法的(是否忘记了“;” ? )
-//错误	C3874	“main”的返回类型应为“int”而非“Foo”	
-//错误	C2440	“return” : 无法从“int”转换为“Foo”	
-
-int main() 
-{
-	Sales_data data1, data2;
-	//交易记录：
-	//0-201-78345-X 3 20.00
-	//0-201-78345-X 2 25.00
-	//double price = 0;
-	data1.bookNo = "0-201-78345-X";
-	data1.units_sold = 3;
-	data1.revenue = 3 * 20.00;
-	data2 = {
-		"0-201-78345-X",
-		2,
-		2*25.00
-	};
-	if (data1.bookNo==data2.bookNo)
-	{
-		unsigned totalCnt = data1.units_sold + data2.units_sold;
-		double totalRevenue = data1.revenue + data2.revenue;
-		cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << endl;
-		if (totalCnt!=0)
-		{
-			cout << totalRevenue / totalCnt << endl;
-		}
-		else
-		{
-			cout << "(no sales)" << endl;
-		}
-		return 0;
-	}
-	else
-	{
-		cerr << "Data must refer to the same ISBN" << endl;
-		return -1;
-	}
-	
-};
+//#include<iostream>
+////#include<string>
+//#include"Sales_data.h"
+//using namespace std;
+//
+//
+//
+////Sales_data accum, trans, * salesptr;
+//
+////练习2.39
+//struct Foo {};
+////当没有写分号时，编译器报了以下错误：
+////错误	C2628	“Foo”后面接“int”是非法的(是否忘记了“;” ? )
+////错误	C3874	“main”的返回类型应为“int”而非“Foo”	
+////错误	C2440	“return” : 无法从“int”转换为“Foo”	
+//
+//int main() 
+//{
+//	Sales_data data1, data2;
+//	//交易记录：
+//	//0-201-78345-X 3 20.00
+//	//0-201-78345-X 2 25.00
+//	//double price = 0;
+//	data1.bookNo = "0-201-78345-X";
+//	data1.units_sold = 3;
+//	data1.revenue = 3 * 20.00;
+//	data2 = {
+//		"0-201-78345-X",
+//		2,
+//		2*25.00
+//	};
+//	if (data1.bookNo==data2.bookNo)
+//	{
+//		unsigned totalCnt = data1.units_sold + data2.units_sold;
+//		double totalRevenue = data1.revenue + data2.revenue;
+//		cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << endl;
+//		if (totalCnt!=0)
+//		{
+//			cout << totalRevenue / totalCnt << endl;
+//		}
+//		else
+//		{
+//			cout << "(no sales)" << endl;
+//		}
+//		return 0;
+//	}
+//	else
+//	{
+//		cerr << "Data must refer to the same ISBN" << endl;
+//		return -1;
+//	}
+//	
+//};
