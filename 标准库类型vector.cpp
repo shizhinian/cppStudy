5ab8e09c29f737f69f0ccc1405d0caf49cd7ba01
XuRui <378007071@qq.com>
2023-08-01 16:51:53 +0800

    study vector

--- 标准库类型vector.cpp
+++ 标准库类型vector.cpp
@@ -1,24 +1,65 @@
-#include<vector>
-#include<string>
-using std::string;
-using std::vector;
-
-int main()
-{
-	vector<int> v1;
-	vector<int>v2(v1);
-	vector<int>v22 = v1;
-	vector<int>v3(10, 1);
-	vector<int>v4(10);
-	vector<int>v5{ 1,2,3,4 };
-	vector<int>v55 = { 1,2,3,4 };
-
-	vector<vector<int>> ivec;
-	//vector<string> svec=ivec;
-	vector<string> svec(10, "null");
-
-	vector<string> v7(10, "hi");
-	vector<string> v8{ 10, "hi" };
-
-	//向vector对象中添加元素
-}
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//using namespace std;
+//using std::string;
+//using std::vector;
+//
+//
+//
+//int main()
+//{
+//	//vector<int> v1;
+//	//vector<int>v2(v1);
+//	//vector<int>v22 = v1;
+//	//vector<int>v3(10, 1);
+//	//vector<int>v4(10);
+//	//vector<int>v5{ 1,2,3,4 };
+//	//vector<int>v55 = { 1,2,3,4 };
+//
+//	//vector<vector<int>> ivec;
+//	////vector<string> svec=ivec;
+//	//vector<string> svec(10, "null");
+//
+//	//vector<string> v7(10, "hi");
+//	//vector<string> v8{ 10, "hi" };
+//
+//	//向vector对象中添加元素
+//	/*vector<int> v2;
+//	for (int i = 0; i!=100; i++)
+//	{
+//		v2.push_back(i);
+//	}*/
+//
+//	////其他vector操作
+//	//vector<int> v{ 1,2,3,4,5,6,7,8 };
+//	//for (auto& i : v)
+//	//	i *= i;
+//	//for (auto i : v)
+//	//	cout << i << " ";
+//	//cout << endl;//out:1 4 9 16 25 36 49 64
+//
+//	/*vector<unsigned>scores(11, 0);
+//	unsigned grade[]= {42,65,95,100,39,67,95,76,88,76,83,92,76,93};*/
+//
+//	/*vector<unsigned> scores(11, 0);
+//	unsigned grade;
+//	while (cin>>grade)
+//	{
+//		if (grade<=100)
+//		{
+//			++scores[grade / 10];
+//		}
+//	}
+//	
+//	auto index = grade / 10;
+//	scores[index] = scores[index] + 1;*/
+//
+//	//vector<int>ivec;//空的vector对象
+//	//for (decltype(ivec.size()) i = 0; i!=10; i++)
+//	//{
+//	//	//ivec[i] = i;编译成功但运行错误。
+//	//	ivec.push_back(i);
+//	//}
+//
+//}
