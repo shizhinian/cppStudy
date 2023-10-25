1013f714cc10b2b0654bd517a4b2af7ea9ad9f43
XuRui <378007071@qq.com>
2023-10-25 16:49:24 +0800

    2023025 noon commit

--- HasPtr.cpp
+++ HasPtr.cpp
@@ -9,6 +9,7 @@
 using namespace::std;
 //赋值运算符通常应该返回一个指向其左侧运算对象的引用
 HasPtr& HasPtr::operator=(const HasPtr& hp) {
+	/*
 	auto newps = new string(*hp.ps);
 	//newps = hp.ps;
 	delete ps;//释放*this.ps旧内存
@@ -16,15 +17,35 @@ HasPtr& HasPtr::operator=(const HasPtr& hp) {
 	//ps = hp.ps;
 	i = hp.i;
 	return *this;
+	*/
+	++*hp.use;
+	if (--*use == 0)
+	{
+		delete ps;
+		delete use;
+	}
+	ps = hp.ps;
+	i = hp.i;
+	use = hp.use;
+	return *this;
+}
+
+HasPtr::~HasPtr(){
+	if (--*use==0)
+	{
+		delete ps;
+		delete use;
+	}
 }
 
 
 int main() {
 	HasPtr hp1("abc");
 
-	HasPtr hp2("def");
+	HasPtr hp2;
 
 	hp2 = hp1;
-
+	cout << "hp1:" << hp1.ps << endl << "hp1:" << hp2.ps;
+	cout <<"hp1:"<< *hp1.use <<endl <<"hp1:"<< *hp2.use;
 	//HasPtr(hp1);
 }
--- HasPtr.h
+++ HasPtr.h
@@ -8,20 +8,24 @@
 using namespace::std;
 
 class HasPtr {
+	friend int main();
 public:
 	//默认构造函数
 	//HasPtr()=default;
 
 	//拷贝构造函数
 	//HasPtr(const string& s=string()):ps(new string(s)),i(0){}
-	HasPtr(const string& s):ps(new string(s)),i(0){}
+	HasPtr(const string& s=string()):ps(new string(s)),i(0),use(new size_t(1)){}
 
 
-	HasPtr(const HasPtr& p) :ps(p.ps), i(p.i) {}
+	//HasPtr(const HasPtr& p) :ps(p.ps), i(p.i) {}
+	HasPtr(const HasPtr& p) :ps(p.ps), i(p.i), use(p.use) { ++*use; }
 
 	//拷贝赋值运算符,如果类未定义自己的拷贝赋值运算符，编译器会生成一个合成拷贝运算符
 	HasPtr& operator=(const HasPtr&);
+	~HasPtr();
 private:
 	string* ps;
 	int i;
+	size_t* use;
 };
